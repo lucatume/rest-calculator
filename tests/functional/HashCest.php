579f1785d837fe38c18649259caf732492ed931d
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-07-14 16:18:24 +0200

    fixed PHP 5.2 compat

--- tests/functional/HashCest.php
+++ tests/functional/HashCest.php
@@ -15,21 +15,4 @@ class HashCest {
 
 		$I->see(restCalculator_hash('aba'));
 	}
-
-	/**
-	 * It should return the filtered hash value
-	 *
-	 * @test
-	 */
-	public function it_should_return_the_filtered_hash_value(FunctionalTester $I) {
-		add_filter('restCalculator_hash', function($value){
-			return $value . 'foo';
-		});
-
-		$I->amOnPage("/wp-json/calc/hash/aba");
-
-		$I->seeResponseCodeIs(200);
-
-		$I->see(restCalculator_hash('aba'));
-	}
 }
